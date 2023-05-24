ab2ced5b842d5b1e51d82ffb40a75287054b1e79
Crowdbotics <team@crowdbotics.com>
2023-05-24 17:48:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tesla-airport-servi-40961",
+  url: "https://tesla-airport-servi-40961.botics.co",
+  api: "https://tesla-airport-servi-40961.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
